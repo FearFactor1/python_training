628f3539ac7342b736db22f6f21ee1f0018d1d09
d.bystrikov <zelezodoroznik@yandex.ru>
2018-11-06 14:23:35 +0300

    добавлен помощник контактов - второй тест не работает

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
-
+from fixture.session import SessionHelperContact
+from fixture.contact import ContactHelper
 
 class Application:
 
@@ -10,7 +11,8 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
+        self.session = SessionHelperContact(self)
+        self.contact = ContactHelper(self)
 
 
     def open_home_page(self):
