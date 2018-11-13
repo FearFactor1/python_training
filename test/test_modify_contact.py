b23a4a06783cd0f623b60a0fbe7cf196364a228f
d.bystrikov <zelezodoroznik@yandex.ru>
2018-11-13 17:04:25 +0300

    Задание №9: Обеспечить выполнение предусловий в тестах для удаления и модификации для групп и контактов - готово

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,14 +3,20 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New name", title="New title"))
 
 
 def test_modify_contact_title(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(title="New title"))
 
 
 def test_modify_contact_spisok(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(bday="9", bmonth="May", byear="2023", aday="7", amonth="April",
                                 ayear="1988"))
 
