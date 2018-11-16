92b6f2599cf130aa4eef256e2907b0e7326286d2
d.bystrikov <zelezodoroznik@yandex.ru>
2018-11-16 18:01:39 +0300

    Unable to locate element: [name="selected[]"]

--- test/Test_Add_New.py
+++ test/Test_Add_New.py
@@ -3,17 +3,21 @@ from model.contact import Contact
 
 
 def test_add_new(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.create(Contact(firstname="ivanov", middlename="vasya", lastname="ivanov", nickname="xhel",
                                title="contakt", company="xshel", address="rete, k-5", home="768867876", mobile="+79445",
                                work="546", fax="546774", email="teretr@ty.ty", email2="gfhf@hg.nb", email3="fggfh@.yu.tyu",
                                homepage="http://treet.rt", bday="2", bmonth="January", byear="1999", aday="6",
                                amonth="March", ayear="2000", address2="gddgdd, d-2. kv4", phone2="city",
                                notes="ghfgfhfhfhfghfghfhfhfg"))
-
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
 def test_add_new_emty(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
                                homepage="", bday="", bmonth="-", byear="", aday="-", amonth="-", ayear="", address2="",
                                phone2="", notes=""))
-
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,18 +5,27 @@ from model.contact import Contact
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(firstname="New name", title="New title"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_title(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(title="New title"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_spisok(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(bday="9", bmonth="May", byear="2023", aday="7", amonth="April",
                                 ayear="1988"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
