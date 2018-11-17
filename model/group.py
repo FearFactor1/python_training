dc4208058b80b958bc6f8ed938ff76a0393d4ac9
dima <zelezodoroznik@yandex.ru>
2018-11-17 16:45:44 +0300

    Проверка равенства объектов и списков

--- model/group.py
+++ model/group.py
@@ -1,3 +1,4 @@
+
 class Group:
 
     def __init__(self, name=None,header=None,footer=None, id=None):
@@ -5,3 +6,11 @@ class Group:
         self.header=header
         self.footer=footer
         self.id=id
+
+
+    def __repr__(self):
+        return "%s:%s" % (self.id.name)
+
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
