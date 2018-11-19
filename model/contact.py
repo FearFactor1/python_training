abeb18d3c32e14b39b1113e03e40a66bca69c0d0
d.bystrikov <zelezodoroznik@yandex.ru>
2018-11-19 11:39:29 +0300

    Задание №11: Реализовать проверки в виде сравнения списков

--- model/contact.py
+++ model/contact.py
@@ -35,11 +35,12 @@ class Contact:
 
 
     def __repr__(self):
-        return "%s:%s" % (self.firstname)
+        return "%s:%s:%s" % (self.id.firstname.lastname)
 
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
+        return (self.id is None or other.id is None or self.id == other.id) or (self.firstname == other.firstname and
+                                                                                self.lastname == other.lastname)
 
 
     def id_or_max(self):
