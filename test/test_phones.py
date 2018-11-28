9bbf0b2b405900b70f9ba562a148a362cda6a6f6
d.bystrikov <zelezodoroznik@yandex.ru>
2018-11-28 09:27:06 +0300

    Клеим строки из частей: прямые и обратные проверки

--- test/test_phones.py
+++ test/test_phones.py
@@ -21,5 +21,5 @@ def clear(s):
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x),
-                                filter(lambda x: x is not None, [contact.homephone, contact.mobilephone, 
+                                filter(lambda x: x is not None, [contact.homephone, contact.mobilephone,
                                                                  contact.workphone, contact.secondaryphone]))))
