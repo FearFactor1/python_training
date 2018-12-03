82bfcc5360bb84700f775ea7a186ff628880b55e
d.bystrikov <zelezodoroznik@yandex.ru>
2018-12-03 16:25:31 +0300

    Отделение тестовых данных от сценариев (DDT продолжается) - для контактов

--- test/Test_Add_New.py
+++ test/Test_Add_New.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [
-    Contact(firstname=firstname, middlename="fghjjf", lastname=lastname, nickname="xhel",
-                               title="contakt", company="xshel", address=address, home="768867876", mobile="+79445",
-                               work="546", fax="546774", email="teretr@ty.ty", email2="gfhf@hg.nb", email3="fggfh@.yu.tyu",
-                               homepage="http://treet.rt", bday="2", bmonth="January", byear="1999", aday="6",
-                               amonth="March", ayear="2000", address2="gddgdd, d-2. kv4", phone2="city",
-                               notes="ghfgfhfhfhfghfghfhfhfg")
-    for firstname in ["", random_string("firstname", 5)]
-    for address in ["", random_string("address", 7)]
-    for lastname in ["", random_string("lastname", 9)]
-]
+from data.add_contact import constant as testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
