781bb58451f55071b970a97a0071f99064a3e5f6
dima <zelezodoroznik@yandex.ru>
2018-12-16 20:22:12 +0300

    Переделка теста емейл

--- test/test_email.py
+++ test/test_email.py
@@ -1,11 +1,10 @@
 import re
 
 
-def test_email_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+def test_email_on_home_page(app,db):
+    contact_from_home_page = db.get_contact_list()[0]
+    contact_from_edit_page = db.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
 
 #def test_emails_on_contact_view_page(app):
 #    contact_from_view_page = app.contact.get_contact_from_view_page(0)
