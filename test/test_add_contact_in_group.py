3536087e5115c891e83a001948860bbca6e079a6
d.bystrikov <zelezodoroznik@yandex.ru>
2018-12-21 09:59:37 +0300

    t

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -1,5 +1,6 @@
 from model.contact import Contact
 import random
+from model.group import Group
 
 
 def test_add_contact_in_group(app, db, check_ui):
@@ -8,7 +9,7 @@ def test_add_contact_in_group(app, db, check_ui):
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     app.contact.add_contact_in_group(contact.id, contact.group_id)
-    new_contacts = db.get_contact_list()
+    new_contacts = db.get_contact_list(Group(id=group_id))
     assert len(old_contacts) == len(new_contacts)
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
